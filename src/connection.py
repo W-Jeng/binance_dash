d25d1d25982c8050be389c5f6b8eb9239e2c3a2d
W-Jeng <woonjeng@gmail.com>
2024-09-18 21:30:24 +0800

    it's running async now

--- src/connection.py
+++ src/connection.py
@@ -3,6 +3,12 @@ import websockets
 import json
 import httpx
 import time
+from dataclasses import dataclass
+
+@dataclass
+class ResponseObject:
+    symbol: str
+    data: dict
 
 class BinanceConnection:
     '''
@@ -15,33 +21,35 @@ class BinanceConnection:
     def __init__(self, symbols):
         self.symbols = symbols
 
-    async def fetch_one(self, semaphore: asyncio.Semaphore, symbol: str) -> None:
+    async def fetch_one(self, client: httpx.AsyncClient,
+                        semaphore: asyncio.Semaphore,
+                        symbol: str) -> None:
         # establish single connection
         print(f'Initiating connection on symbol: {symbol}')
         params = {
             'symbol': symbol,
             'limit': self.MARKET_DEPTH 
         }
-        async with httpx.AsyncClient() as client:
+        async with semaphore:
             response = await client.get(self.BASE_URL, params=params)    
             response.raise_for_status()
 
-        return response.json()
+        return ResponseObject(symbol=symbol, data=response.json())
 
-    async def fetch_multiple(self, symbols: str) -> None:
+    async def fetch_multiple(self) -> None:
         semaphore = asyncio.Semaphore(self.MAX_CONCURRENT)
-        tasks = [self.fetch_one(semaphore, symbol) for symbol in symbols]
-
-        for coroutine_obj in asyncio.as_completed(tasks):
-            resp = await coroutine_obj
-            print(resp)
+        async with httpx.AsyncClient() as client:
+            tasks = [self.fetch_one(client, semaphore, symbol) for symbol in self.symbols]
+            for coroutine_obj in asyncio.as_completed(tasks):
+                resp = await coroutine_obj
+                print(resp)
         return
 
 if __name__ == '__main__':
     start_time = time.time()
-    symbols = ['BTCUSDT', 'ETHUSDT']
+    symbols = ['BTCUSDT', 'ETHUSDT', 'BNBUSDT', 'XRPUSDT', 'SOLUSDT']
     binance_connection = BinanceConnection(symbols)
-    asyncio.run(binance_connection.fetch_multiple(symbols))
+    asyncio.run(binance_connection.fetch_multiple())
     end_time = time.time()
     print(f'Time taken: {round(end_time-start_time,4)}')
 
